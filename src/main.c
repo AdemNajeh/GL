50d7ebf376e14839b14e2349adacd0bc28d221d8
AdemNajeh <adem.najeh@u-psud.fr>
2018-11-15 19:26:40 +0100

    Exo 44 fini

--- src/main.c
+++ src/main.c
@@ -7,3 +7,18 @@ int main(void)
 {
     return 0;
 }
+#include <ansi_c.h>
+#include <userint.h>
+#include "TP_lib.h"
+#include "cvi_gui.h"
+
+void main(void)
+{
+   short resultat, i=0;
+   while(i<10)
+   {
+       resultat= LancerDe();
+       printf("numero %d ",resultat) ;
+       i++;
+   }
+}
