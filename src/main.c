b1ecf1d09a8c20af03cd4e54a71c28dd9f4c04dd
adem najeh <adem.najeh@ge1.iut-cachan.u-psud.fr>
2018-11-27 09:47:37 +0100

    main

--- src/main.c
+++ src/main.c
@@ -7,7 +7,21 @@ int main(void)
 {
 	unsigned char win[6] { 1,2,9,45,65,78 } ;
 	unsigned char joueur[6] {2,6,4,56,89,65} ;
-	
+	int i,W,points;
+
+
+    for (i=0;i<6;i++)
+     {
+         for (W=0;W<6;W++)
+        {
+            if(win[i] == joueur[W])
+            {
+                points++;
+                
+            }
+        }
+        
+     }
 		
 	
 	
