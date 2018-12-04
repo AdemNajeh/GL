9ca7c0761f019d09b6d4997bbeb5aecd7f678457
adem najeh <adem.najeh@ge1.iut-cachan.u-psud.fr>
2018-12-04 09:38:15 +0100

    Exo79

--- src/main.c
+++ src/main.c
@@ -1,9 +1,27 @@
 //  Copyright (c) 2018 Antoine Tran Tan
 //
 
+#include <ansi_c.h>
 #include "my_header.h"
 
 int main(void)
 {
+
+	 char prenom[20];
+	 char nom[20];
+	 int annee_de_naissance;
+	 int age;  
+	 
+	scanf("%s",prenom);
+	scanf("%s",nom);
+	scanf("%d",&anne_de_naissance);
+	
+	 
+	
+	age = 2018- anne_de_naissance;
+	
+	printf("je m'appelle  %s %s et j'ai %d ans" ,prenom,nom,age);
+	
+    
     return 0;
 }
