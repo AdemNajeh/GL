94a302f52613cd3def90cb73c95a67c3e519f77d
AdemNajeh <adem.najeh@u-psud.fr>
2018-12-06 19:51:38 +0100

    j'avais oublier le MessagePopup

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 //  Copyright (c) 2018 Antoine Tran Tan
 //
 
+#include <userint.h>
 #include <ansi_c.h>
 #include "my_header.h"
 
@@ -9,6 +10,7 @@ int main(void)
 
 	 char prenom[20];
 	 char nom[20];
+	 char info[50];
 	 int annee_de_naissance;
 	 int age;  
 	 
@@ -20,8 +22,8 @@ int main(void)
 	
 	age = 2018- annee_de_naissance;
 	
-	printf("je m'appelle  %s %s et j'ai %d ans" ,prenom,nom,age);
+	sprintf(info,"je m'appelle  %s %s et j'ai %d ans" ,prenom,nom,age);
+	MessagePopup("info",info);
 	
-    
     return 0;
 }
