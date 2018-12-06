521b5a6e8ed64e1dc1b7d415c05217605410c0bc
AdemNajeh <adem.najeh@u-psud.fr>
2018-12-06 19:44:58 +0100

    Exo82fini

--- src/main.c
+++ src/main.c
@@ -1,45 +1,34 @@
-#include "my_header.h"
-#include "TP_lib_suite.h"
+#include <userint.h>
+#include <ansi_c.h>
 
 
-int main (void)
+
+void main(void)
 {
-	unsigned char combinaison_w[6];
-	unsigned char combinaison_p[6] = {10,12,14,93,2,70};
-	unsigned char val_exactes = 0;
-	unsigned char nb_win = 0;
-	unsigned char i,b,c;
+    char ligne1[100] = "Nom : triangle_A" ;
+	char ligne2[100] = "Nb points : 3" ;
+	char ligne3[100] = "Cordonnees: 10 20 30 20 03 40 -1 -1 -1 -1" ;
+	
+	char ligne4[100] = "Nom: pentagone_B" ;
+	char ligne5[100] = "Nb points: 5" ;
+	char ligne6[100] = "Cordonnees : 10 20 30 20 30 40 43 53 35 65" ;
+	
+	char nom_fig[100];
+	char message[100];
+	long x[5];
+	long y[5];
+	int nb_pt;
+	
+	if (ligne3[50]==0)
+		  MessagePopup("erreur","coordonnee manquante");
+	
+	 sscanf(ligne1," %*s %*s %s ",nom_fig);
+	 sscanf(ligne2," %*s %*s %*s %d",&nb_pt);
+	 sscanf(ligne3," %*s %*s %d %*s %d %*s %d %*s %d %*s %d %*s ",&x[0],&x[1],&x[2],&x[3],&x[4]); 
+	 sscanf(ligne3," %*s %*s %*d %d %*d %d %*d %d %*d %d %*d %d ",&y[0],&y[1],&y[2],&y[3],&y[4]); 
+	 
+	 sprintf(message,"la %s possede %d sommets",nom_fig,nb_pt);
+	 MessagePopup("test",message);
+	
 
-
-	for (c=0; c<100; c++)
-	{
-		initialiserTirage();
-
-		for (b=0; b<6; b++)
-		{
-			combinaison_w[b] = tirerNumero();
-		}
-
-		for (i=0; i<6; i++)
-		{
-			for (b=0; b<6; b++)
-			{
-				if(combinaison_w[i] == combinaison_p[b])
-				{
-					val_exactes++;
-				}
-			}
-
-		}
-
-		if(nb_win < val_exactes)
-		{
-			nb_win = val_exactes;
-		}
-
-		val_exactes = 0;
-	}
-
-	return 0;
 }
-
