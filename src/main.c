a2410c1b20f371f0d5bad5a20335902b5d280c6d
AdemNajeh <39296580+AdemNajeh@users.noreply.github.com>
2018-12-18 09:08:10 +0100

    exo25 fini

--- src/main.c
+++ src/main.c
@@ -1,9 +1,33 @@
 //  Copyright (c) 2018 Antoine Tran Tan
 //
 
-#include "my_header.h"
+#include <ansi_c.h>
 
 int main(void)
+
 {
-    return 0;
+	unsigned int a,b,c;
+	int R;
+	
+	R = ( 10 * a + b) * pow(10,c );
+	
+	printf("Indiquer la valeur du premier anneau : ") ;
+	scanf("%u",&a);
+	
+	printf("Indiquer la valeur du du second anneau : ") ;
+	scanf("%u",&b);
+	
+	printf("Indiquer la valeur du troisieme anneau : ") ;
+	scanf("%u",&c);
+	
+	if (c > 6)
+		R = -1 ;
+	
+	else
+			R = ( 10 * a + b) * pow(10,c );
+	
+	printf("La valeur de la resistance est de %d",R);
+		
+
+	return 0 ;
 }
